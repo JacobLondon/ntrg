0ad13bbb8963efb92e260e026ccbd9c7345c910e
Jacob <jelondon12@gmail.com>
2019-12-23 10:50:50 -0800

    adding door selection

--- src/room.c
+++ src/room.c
@@ -33,3 +33,27 @@ void room_draw(struct room *self)
         nce_write(self->buf[i], self->x, self->y + i);
     }
 }
+
+static int rand_choice(int min, int max)
+{
+    int choice = rand() % (max - min) + min;
+    return choice;
+}
+
+void room_gen_door(struct room *self)
+{
+    // choose wall
+    int wallx, wally, choice = rand_choice(0, 3);
+    switch (choice) {
+    case 0: wallx = 0;       wally = rand_choice(0, self->h); // left wall
+    case 1: wallx = self->w; wally = rand_choice(0, self->h); // right wall
+    case 2: wallx = rand_choice(0, self->w); wally = 0;       // top wall
+    case 3: wallx = rand_choice(0, self->w); wally = self->h; // bottom wall
+    default:
+        fprintf(stderr, "Invalid random choice %d\n", choice);
+        exit(-1);
+    }
+    
+    
+
+}
--- src/room.h
+++ src/room.h
@@ -6,9 +6,19 @@
 struct room {
     int x, y, w, h;
     char buf[ROOM_SIZE][ROOM_SIZE];
+    int doors[4], door_dir[4];
+};
+
+enum direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
 };
 
 struct room *room_new(int x, int y, int w, int h);
 void room_draw(struct room *self);
+
+void room_gen_door(struct room *self);
 
 #endif // NTRG_ROOM_H
